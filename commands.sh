e242c118576afe418d3da1bb5a22570159a11d3c
Michael Reynolds <michael4reynolds@gmail.com>
2018-09-17 13:05:37 -0400

    create Ingress config

--- commands.sh
+++ commands.sh
@@ -13,6 +13,7 @@ kubectl delete service client-node-service
 
 # kubectl delete daemonsets,replicasets,services,deployments,pods,rc --all
 
+# Run each time yaml file(s) change
 ls k8s
 kubectl apply -f k8s
 kubectl get pods
@@ -21,9 +22,14 @@ kubectl get pvc
 kubectl get deployments
 kubectl get services
 kubectl logs <pod>-<name>
+###################################
 
 kubectl get storageclass
 kubectl describe storageclass
 
 kubectl create secret generic pgpassword --from-literal PGPASSWORD=12345asdf
 kubectl get secrets
+
+# Add ingress-nginx with Docker form Mac and GCE
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
